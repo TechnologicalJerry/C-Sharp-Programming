fa117cc6538f745bb259dd594e4b432d376f1f18
technologicaljerry <cserajatverma@gmail.com>
2022-08-08 21:26:19 +0530

    update

---
+++ Print.cs
@@ -0,0 +1,13 @@
+using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.Write("Hello World,");
+        Console.Write("How are you?");
+        Console.WriteLine();
+        Console.WriteLine("Hello World");
+        Console.WriteLine("How are you?");
+        Console.WriteLine("Hello World\nHow are you?");
+    }
+}
