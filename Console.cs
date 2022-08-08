56a04030f78e0c0c68ddc4de3a1c8deffc5458ba
technologicaljerry <cserajatverma@gmail.com>
2022-08-08 21:27:27 +0530

    update

---
+++ Console.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace IncludeHelp
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("This is line1");
+            Console.WriteLine("This is line2");
+            Console.Write("This is line3");
+            Console.Write("This is line4");
+
+            Console.ReadLine();
+        }
+    }
+}
