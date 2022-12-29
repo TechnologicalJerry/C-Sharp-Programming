14bef033285a4f7eadf30be5b9facd382dfacb1c
technologicaljerry <cserajatverma@gmail.com>
2022-12-30 00:01:55 +0530

    Update inheritance.cs

--- inheritance.cs
+++ inheritance.cs
@@ -1,28 +1,24 @@
-class Vehicle  // base class (parent) 
+class Vehicle
 {
-    public string brand = "Ford";  // Vehicle field
-    public void honk()             // Vehicle method 
+    public string brand = "Ford";
+    public void honk()
     {
         Console.WriteLine("Tuut, tuut!");
     }
 }
-
-class Car : Vehicle  // derived class (child)
+class Car : Vehicle
 {
-    public string modelName = "Mustang";  // Car field
+    public string modelName = "Mustang";
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Create a myCar object
         Car myCar = new Car();
 
-        // Call the honk() method (From the Vehicle class) on the myCar object
         myCar.honk();
 
-        // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
         Console.WriteLine(myCar.brand + " " + myCar.modelName);
     }
 }
