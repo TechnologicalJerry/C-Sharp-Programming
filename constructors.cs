34d2de0fd05f85ed3b781d2ae3b44cfe57271d58
technologicaljerry <cserajatverma@gmail.com>
2022-12-30 00:01:10 +0530

    Update constructors.cs

--- constructors.cs
+++ constructors.cs
@@ -7,8 +7,6 @@ namespace MyApplication
         public string model;
         public string color;
         public int year;
-
-        // Create a class constructor with multiple parameters
         public Car(string modelName, string modelColor, int modelYear)
         {
             model = modelName;
