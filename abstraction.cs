eb96c0bf3794b6ba2073cd1840c70b72f5a6d044
technologicaljerry <cserajatverma@gmail.com>
2023-01-04 23:06:49 +0530

    Update abstraction.cs

--- abstraction.cs
+++ abstraction.cs
@@ -2,24 +2,19 @@ using System;
 
 namespace MyApplication
 {
-    // Abstract class
     abstract class Animal
     {
-        // Abstract method (does not have a body)
         public abstract void animalSound();
-        // Regular method
         public void sleep()
         {
             Console.WriteLine("Zzz");
         }
     }
 
-    // Derived class (inherit from Animal)
     class Pig : Animal
     {
         public override void animalSound()
         {
-            // The body of animalSound() is provided here
             Console.WriteLine("The pig says: wee wee");
         }
     }
@@ -28,7 +23,7 @@ namespace MyApplication
     {
         static void Main(string[] args)
         {
-            Pig myPig = new Pig();  // Create a Pig object
+            Pig myPig = new Pig();
             myPig.animalSound();
             myPig.sleep();
         }
