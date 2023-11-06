491e8aac4d47cb98b6e35edb48bb4738c7659949
technologicaljerry <cserajatverma@gmail.com>
2023-11-06 23:58:26 +0530

    Update files.cs

--- files.cs
+++ files.cs
@@ -9,7 +9,6 @@ namespace MyApplication
         {
             string writeText = "Hello World!";
             File.WriteAllText("filename.txt", writeText);
-
             string readText = File.ReadAllText("filename.txt");
             Console.WriteLine(readText);
         }
